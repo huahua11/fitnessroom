5d365a10b9213c9a56dfa1f7e2ce3cf7e02c063d
17634738693 <291407656@qq.com>
2020-09-09 09:50:30 +0800

    xixixi

---
+++ src/main/java/com/aaa/fitnessroom/FitnessroomApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.fitnessroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessroomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aaa/fitnessroom/FitnessroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.fitnessroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
